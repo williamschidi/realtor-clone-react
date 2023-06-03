e1d1af59e3cca45eecd3ff580cb64b4d5b4ec3c1
williamschidi <williamchidi1@gmail.com>
2023-06-03 23:08:45 +0100

    Install reatjs and tailwind css and create the first template

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { BrowserRouter } from 'react-router-dom';
+// import { UserProvider } from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <UserProvider> */}
+    {/* <BrowserRouter> */}
     <App />
+    {/* </BrowserRouter> */}
+    {/* </UserProvider> */}
   </React.StrictMode>
 );
 
